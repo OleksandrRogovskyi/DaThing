20ea4194416f4c7b6f698cefa34778edec70f6e9
OleksandrRogovskyi <alex.rogovskiy.kiev@gmail.com>
2025-10-06 18:18:50 +0100

    time picker

--- Assets/Code/UI/UITimePicker.cs
+++ Assets/Code/UI/UITimePicker.cs
@@ -3,29 +3,45 @@ using UnityEngine.EventSystems;
 
 public class UITimePicker : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
+    public UnityEngine.Events.UnityEvent onValueChanged;
+
+    [SerializeField] int count;
+    [SerializeField] GameObject timeText;
+    [SerializeField] private Transform container;
+
     private bool isDragging = false;
     private Vector2 dragStartPos;
-    private float deltaY;
+    private Vector3 containerStartPos;
 
-    [SerializeField] private Transform hours;
+    private void Start()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            var timeItem = Instantiate(timeText, container);
+            timeItem.GetComponent<TMPro.TextMeshProUGUI>().text = i.ToString("D2");
+        }
+
+        var timeItemHeight = timeText.GetComponent<RectTransform>().rect.height;
+        container.localPosition = new Vector3(0, -count * timeItemHeight / 2 + timeItemHeight / 2, 0);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isDragging = true;
+        dragStartPos = eventData.position;
+        containerStartPos = container.localPosition;
+    }
 
     public void OnDrag(PointerEventData eventData)
     {
-        if (!isDragging)
-        {
-            dragStartPos = eventData.pressPosition;
-            isDragging = true;
-        }
+        if (!isDragging) return;
 
         Vector2 currentPos = eventData.position;
-        deltaY = currentPos.y - dragStartPos.y;
+        float deltaY = currentPos.y - dragStartPos.y;
 
-        hours.localPosition = new Vector3(hours.localPosition.x, deltaY, hours.localPosition.z);
+        container.localPosition = containerStartPos + new Vector3(0, deltaY, 0);
     }
-    public void OnPointerDown(PointerEventData eventData)
-    {
 
-    }
     public void OnPointerUp(PointerEventData eventData)
     {
         isDragging = false;
